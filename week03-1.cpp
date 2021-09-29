a727eaeed919c290525dbdb10d13a56dcfab869c
kingdog2 <91170825+kingdog2@users.noreply.github.com>
2021-09-29 12:11:18 +0800

    Add files via upload

---
+++ week03-1.cpp
@@ -0,0 +1,5 @@
+///©ñ¤jÁY¤p:ctrl+¾¦½ü
+#include <stdio.h>
+int main(){
+    printf("Hello World");
+}
---
+++ week03-4.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello Wolrd\n");
+    printf("%d", 300); ///有個十進位數字
+}
