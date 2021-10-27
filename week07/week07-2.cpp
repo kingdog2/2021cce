1c29566bc121962a3e61c95e22053d455b00ec03
kingdog2 <91170825+kingdog2@users.noreply.github.com>
2021-10-27 12:01:51 +0800

    Add files via upload

---
+++ week07/week07-2.cpp
@@ -0,0 +1,14 @@
+///複習if(判斷) 及新的 while(迴圈)
+#include <stdio.h>
+int main()
+{
+    int n=10;
+
+    // if(n>0){
+    //    printf("n大於0\n");
+    //}
+
+    while(n>0){
+        printf("n大於0\n");
+    }
+}
---
+++ week07/week07-3.cpp
@@ -0,0 +1,11 @@
+///while(°j°é) °tn=n-1
+#include <stdio.h>
+int main()
+{
+    int n=2;
+
+    while(n>0){
+        printf("n¤j©ó0\n");
+        n = n - 1;
+    }
+}
