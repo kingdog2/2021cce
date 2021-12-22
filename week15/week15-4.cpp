75265188f73d03ad3064af424e1ef8ac8df3c5b7
kingdog2 <91170825+kingdog2@users.noreply.github.com>
2021-12-22 11:57:15 +0800

    Add files via upload

---
+++ week15/week15-4.cpp
@@ -0,0 +1,15 @@
+///輾轉相除法(用函式呼叫函式)
+#include <stdio.h>
+int box(int a, int b)
+{
+    if(a==0)return b;
+    if(b==0)return a;
+    return box(b, a%b);
+}
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a, &b);
+    int ans= box(a,b);
+    printf("%d", ans);
+}
